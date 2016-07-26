49e2b6d16f713aea69723325d757f9dd30be787b
Doug Cole <idugcoal@gmail.com>
2016-07-26 12:16:10 -0700

    adds logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './handcuffs.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="Appy-logo" alt="logo" />
+          <h2>Welcome to Crime Time</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          TESTING 123
         </p>
       </div>
     );
